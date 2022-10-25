c463ecbdad3b7bd658e61128a567a9fd809e7bc4
Josh <joshuamyers1097@gmail.com>
2022-10-25 18:17:02 +0100

    particles now draw

--- script.js
+++ script.js
@@ -24,13 +24,31 @@ window.addEventListener('load', function() {
         constructor() {
             this.x = 0;
             this.y = 0;
-            this.size = 3;
+            this.size = 30;
+        }
+        draw() {
+            ctx.fillRect(this.x, this.y, this.size, this.size)
         }
     }
 
     class Effect {
+        constructor(width, height){
+            this.width = width;
+            this.height = height;
+            this.particlesArray = [];
+        }
+        init(){
+            this.particlesArray.push(new Particle());
+        }
+        draw(){
+            this.particlesArray.forEach(particle => particle.draw())
 
+        }
     }
+
+    const effect = new Effect(canvas.width, canvas.height);
+    effect.init();
+    effect.draw();
 
     function animate() {
 

650bbf8e297f488a1e8813e17a64727346c21358
jkaar <96846917+jkaar@users.noreply.github.com>
2022-10-30 20:48:50 +0000

    pixel analysis

--- script.js
+++ script.js
@@ -7,10 +7,13 @@ window.addEventListener('load', function() {
     
 
     class Particle {
-        constructor(effect) {
+        constructor(effect, x, y, colour) {
             this.effect = effect;
             this.x = Math.random() * this.effect.width;
             this.y = Math.random() * this.effect.height;
+            this.originX = Math.floor(x);
+            this.originY = Math.floor(y);
+            this.colour = colour;
             this.size = 30;
             this.vx = 1;
             this.vy = 1;
@@ -34,16 +37,31 @@ window.addEventListener('load', function() {
             this.centerY = this.height * 0.5;
             this.x = this.centerX - this.image.width * 0.5;
             this.y = this.centerY - this.image.height * 0.5;
+            // this.gap is used to pixelate the image to improve performance
+            this.gap = 5;
         }
-        init(){
-            for(let i = 0; i<10; i++ ){
-                this.particlesArray.push(new Particle(this));
+        init(context){
+            context.drawImage(this.image, this.x, this.y)
+            // points to Uint8ClampedArray with the pixel data
+            const pixels = context.getImageData(0, 0, this.width, this.height).data;
+            for(let y = 0; y < this.height; y += this.gap){
+                for(let x = 0; x < this.width; y += this.gap){
+                    // mapping linear number to x and y coordinates
+                    const index = (y * this.width + x) * 4;
+                    const red = pixels[index];
+                    const green = pixels[index + 1];
+                    const blue = pixels[index + 2];
+                    const alpha = pixels[index + 3];
+                    const colour = 'rgb('+ red + ','+ green + ','+ blue + ')';
+
+                    if (alpha > 0){
+                        this.particlesArray.push(new Particle(this, x, y, colour))
+                    }
+                }
             }
-            
         }
         draw(context){
             this.particlesArray.forEach(particle => particle.draw(context));
-            context.drawImage(this.image, this.x, this.y);
         }
         update(){
             this.particlesArray.forEach(particle => particle.update());
@@ -51,7 +69,7 @@ window.addEventListener('load', function() {
     }
 
     const effect = new Effect(canvas.width, canvas.height);
-    effect.init();
+    effect.init(ctx);
     
     
 
@@ -61,6 +79,6 @@ window.addEventListener('load', function() {
         effect.update();
         requestAnimationFrame(animate);
     }
-    animate();
+    //animate();
 
 });
